46502271298f527d57b2dbebf2a6d3b640570c25
unknown <jonathanlaguado99@gmail.com>
2022-06-19 21:21:57 -0500

    add get query and getid

--- api/src/controllers/videogame.js
+++ api/src/controllers/videogame.js
@@ -3,39 +3,79 @@ const { API_KEY } = process.env;
 const axios = require("axios");
 const { Op, Videogame } = require("../db");
 
+const formatGame = async (collection) => {
+  try {
+    let request = await axios(`https://api.rawg.io/api/games${API_KEY}`);
+    request = request.data;
+    while (request.next && collection.length < 24) {
+      collection.push(
+        request.results.map((game) => {
+          return {
+            name: game.name,
+            platforms: { ...game.platforms },
+            rating: { ...game.ratings },
+            image: game.background_image,
+            released: game.released,
+            description: `https://api.rawg.io/api/games/${game.id}${API_KEY}`,
+          };
+        })
+      );
+      request = await axios(request.next);
+      request = request.data;
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
   getAll: async (req, res) => {
+    // videogames/query o videogames/
     try {
-      let videoGames = await Videogame.findAll();
-      if (videoGames.length) {
-        return res.json(videoGames);
+      //--------------------query-----------------------------
+      let { name } = req.query;
+      if (name) {
+        let search = await Videogame.findAll({
+          where: {
+            name: {
+              [Op.iLike]: `%${name}%`,
+            },
+          },
+        });
+        if (!search.length) {
+          throw new Error("no se encontraron games");
+        }
+        return res.json(search);
       }
-      let request = await axios(`https://api.rawg.io/api/games${API_KEY}`);
-      request = request.data;
-
-      while (request.next && videoGames.length < 25) {
-        videoGames.push(
-          request.results.map((game) => {
-            return {
-              name: game.name,
-              platforms: { ...game.platforms },
-              rating: { ...game.ratings },
-              image: game.background_image,
-              released: game.released,
-              description: `https://api.rawg.io/api/games/${game.id}${API_KEY}`,
-            };
-          })
-        );
-        request = await axios(request.next);
-        request = request.data;
+      //---------------------- / -----------------------------
+      let games = await Videogame.findAll();
+      if (games.length) {
+        return res.json(games);
       }
 
-      for (games of videoGames) {
-        await Videogame.bulkCreate(games);
+      await formatGame(games);
+
+      for (gamesCollection of games) {
+        await Videogame.bulkCreate(gamesCollection);
       }
-      return res.json(await Videogame.findAll());
+      return res.end();
     } catch (err) {
-      return res.json(err);
+      return res.status(404).json(err.message);
+    }
+  },
+  getId: async (req, res) => {
+    try {
+      let { id } = req.params;
+      if (!id) {
+        throw new Error("id requerido");
+      }
+      let videogame = await Videogame.findByPk(id);
+      if (!videogame) {
+        throw new Error("videogame no encontrado");
+      }
+      return res.json(videogame);
+    } catch (err) {
+      return res.status(404).json({ message: err.message });
     }
   },
 };
--- api/src/routes/videogame.js
+++ api/src/routes/videogame.js
@@ -3,5 +3,5 @@ const app = express.Router();
 const controller = require("../controllers/videogame");
 
 app.get("/", controller.getAll);
-
+app.get("/:id", controller.getId);
 module.exports = app;
