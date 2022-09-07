7afac5501f304515c5ddeb3a2d9b8d3664afdaf3
unknown <jonathanlaguado99@gmail.com>
2022-09-07 15:01:27 -0500

    config vercel

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,1 +1,2 @@
-export const baseUrl = "https://labgamepi.herokuapp.com";
+//export const baseUrl = "https://labgamepi.herokuapp.com";
+export const baseUrl = "https://pi-videogames-main-production.up.railway.app";
