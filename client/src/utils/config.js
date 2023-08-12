2a4360f58036f93228c091125ecabac7b1af1ce5
Jonathan Julian Laguado L <jonathanlaguado99@gmail.com>
2023-08-12 13:09:57 -0500

    [Feat]: amplify config

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,2 +1,3 @@
 //export const baseUrl = "https://labgamepi.herokuapp.com";
-export const baseUrl = "https://pi-videogames-main-production.up.railway.app";
+//export const baseUrl = "https://pi-videogames-main-production.up.railway.app";
+export const baseUrl = "https://main.drbzv0w3mvycv.amplifyapp.com";
